50077c83d7c7076238120e6a345075ba00f59321
KALAI SELVAN KRISHNAMOORTHY <76065108+KalaiKrish1210@users.noreply.github.com>
2022-08-30 01:28:08 +0530

    Add files via upload

---
+++ Variable.js
@@ -0,0 +1,21 @@
+let numOne = 12, num_two = 11
+// declared the varible name to acheived the camel case and snake case
+let total = numOne + num_two
+console.log(total)
+
+let userName = "Kalai"
+// String declaration we can use either double coute or single
+console.log(userName)
+
+userName = "Kalai's father name Krish"
+// if we want to use the single coute inside of the sentence that we need to go with double coute
+// If you already declared the variable as numone we don’t want the “let” keyword again. Else it will give the error.
+console.log(userName)
+
+// area of circle calculation
+let radius = 8
+const pi = 3.14 
+// pi value is not changeable so we need to use the "const" keyword
+
+// Formula pir2
+console.log(pi*radius*radius)
---
+++ dataTypes.js
@@ -0,0 +1,18 @@
+ let num1 = 12 
+ // It represents the number
+ let user = "Kalai"
+ // It represents the String
+ let num2 = 1.1
+ // It represents the floating point
+ let num3 = 123456789n
+// If you mentioned the "n" at last of the digit it will consider as bigint
+
+let num4 = 5<6
+// We boolean type like above 
+console.log(num4)
+
+let num5 = null
+// It represent the null type
+
+console.log(typeof num2)
+// Use of this "typeof" method we can find the data type is which one.
