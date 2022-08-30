00c973e6f4d0565dd58d8bf12bc017efe06c708d
KALAI SELVAN KRISHNAMOORTHY <76065108+KalaiKrish1210@users.noreply.github.com>
2022-08-31 03:09:42 +0530

    Add files via upload

---
+++ operators.js
@@ -0,0 +1,59 @@
+let num1 = 12, num2 = 11
+let result
+// Arithmetic operators
+
+    //Addition
+    result = num1 + num2
+    console.log(result)
+
+    //Subraction
+    result = num1 - num2
+    console.log(result)
+
+    //Multiplication
+    result = num1 * num2
+    console.log(result)
+
+    //Divide
+    result = num1 / num2
+    console.log(result)
+
+    //Modulo
+    result = num1 % num2
+    console.log(result)
+
+    //Short hand operators
+    num1 += 11 // num1 = num1 + 12
+    console.log (num1)
+
+    // Increamnet or decreament 
+    num1--
+    console.log(num1)
+
+    num2++
+    console.log(num2)
+
+// Relational operators
+let relnum = 7>6
+console.log(relnum)
+
+    //string comparison
+    let x = 'Kalai', y = 'Selvan'
+    let data = x>y
+    console.log(data)
+    // it is comparing the ASCII values of the alphabets then it will shows the results
+
+// Logical operator
+let i = 12, j = 11, k = 15
+
+    //AND
+    let result1 = i>j && i < k // Both codition will be satisfied then only it will be true
+    console.log(result1)
+
+    //OR
+    result1 = i>j || i < k // Any one condition will be satisfy then it will be true
+    console.log(result1)
+
+    //NOT
+    let result2 = !result1 // it just reverse the what ever we get.
+    console.log(result2)
