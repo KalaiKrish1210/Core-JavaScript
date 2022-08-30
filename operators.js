7b2d13c9a5f64cbd7c839c54699dfcd7fea52a50
KALAI SELVAN KRISHNAMOORTHY <76065108+KalaiKrish1210@users.noreply.github.com>
2022-08-31 02:32:07 +0530

    Add files via upload

---
+++ operators.js
@@ -0,0 +1,32 @@
+let num1 = 12, num2 = 11
+let result
+//Addition
+result = num1 + num2
+console.log(result)
+
+//Subraction
+result = num1 - num2
+console.log(result)
+
+//Multiplication
+result = num1 * num2
+console.log(result)
+
+//Divide
+result = num1 / num2
+console.log(result)
+
+//Modulo
+result = num1 % num2
+console.log(result)
+
+//Short hand operators
+num1 += 11 // num1 = num1 + 12
+console.log (num1)
+
+// Increamnet or decreament 
+num1--
+console.log(num1)
+
+num2++
+console.log(num2) 
